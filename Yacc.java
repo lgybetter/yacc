0b846e201a30fde7e9cdede7d242018fe0575bf0
lgybetter <437675103@qq.com>
2017-06-16 23:21:35 +0800

    start to build LL1Table

--- Yacc.java
+++ Yacc.java
@@ -9,16 +9,21 @@ public class Yacc {
       this.name = name;
     }
 
+    public Node(String name, ArrayList<String[]> childs) {
+      this.name = name;
+      this.childs = childs;
+    }
+
     public ArrayList<String[]> getChilds() {
       return this.childs;
     }
 
     public void split(String childs) {
       String[] childArray = childs.split("\\|");
-      String [] temp = {"ε"};
+      String[] temp = { "ε" };
       for (int i = 0; i < childArray.length; i++) {
         String[] test = childArray[i].split("><|\"<|>\"|\"\"");
-        if(test.length <= 0) {
+        if (test.length <= 0) {
           test = temp;
         }
         for (int j = 0; j < test.length; j++) {
@@ -34,9 +39,12 @@ public class Yacc {
     private HashMap<String, Node> bnf = new HashMap();
     private HashMap<String, HashSet<String>> first = new HashMap();
     private HashMap<String, HashSet<String>> follow = new HashMap();
-    private HashSet<String> followToFind = new HashSet<>();
     private String start = "";
+    private HashSet<String> terminator = new HashSet<>();
+    private HashSet<String> notTerminator = new HashSet<>();
     private ArrayList<String> mutilChoose = new ArrayList<>();
+    private HashMap<String, HashMap<String, ArrayList<String[]>>> ll1Table = new HashMap<>();
+
     public Boolean isTerminator(String key) {
       if (this.bnf.containsKey(key)) {
         return false;
@@ -55,8 +63,8 @@ public class Yacc {
         for (int j = 0; j < childs.get(i).length; j++) {
           if (this.isTerminator(childs.get(i)[j])) {
             // if (childs.get(i)[j] != "ε") {
-              firstSet.add(childs.get(i)[j]);
-              break;
+            firstSet.add(childs.get(i)[j]);
+            break;
             // }
           } else {
             HashSet<String> tempFirstSet = this.getFirstSet(childs.get(i)[j]);
@@ -75,7 +83,7 @@ public class Yacc {
       Iterator iter = this.bnf.keySet().iterator();
       while (iter.hasNext()) {
         String key = iter.next().toString();
-        if(!this.first.containsKey(key)) {
+        if (!this.first.containsKey(key)) {
           HashSet<String> value = this.getFirstSet(key);
           this.first.put(key, value);
         }
@@ -83,35 +91,34 @@ public class Yacc {
       return this.first;
     }
 
-
     public HashSet<String> getFollowSet(String key) {
       HashSet<String> followSet = new HashSet<>();
       Iterator itr = this.bnf.keySet().iterator();
-      while(itr.hasNext()) {
+      while (itr.hasNext()) {
         String _key = itr.next().toString();
         Node node = this.bnf.get(_key);
         ArrayList<String[]> childs = node.getChilds();
-        for(int i = 0; i < childs.size(); i++) {
-          for(int j = 0; j < childs.get(i).length; j++) {
-            if(childs.get(i)[j].equals(key)) {
-              if(j < childs.get(i).length - 1) {
-                if(this.isTerminator(childs.get(i)[j+1])) {
-                  followSet.add(childs.get(i)[j+1]);
+        for (int i = 0; i < childs.size(); i++) {
+          for (int j = 0; j < childs.get(i).length; j++) {
+            if (childs.get(i)[j].equals(key)) {
+              if (j < childs.get(i).length - 1) {
+                if (this.isTerminator(childs.get(i)[j + 1])) {
+                  followSet.add(childs.get(i)[j + 1]);
                 } else {
-                  HashSet<String> tempFollowSet = this.first.get(childs.get(i)[j+1]);
+                  HashSet<String> tempFollowSet = this.first.get(childs.get(i)[j + 1]);
                   Iterator _itr = tempFollowSet.iterator();
-                  while(_itr.hasNext()) {
+                  while (_itr.hasNext()) {
                     followSet.add(_itr.next().toString());
                   }
                 }
               } else {
-                if(!_key.equals(key)) {
-                  if(_key.equals(this.start)) {
+                if (!_key.equals(key)) {
+                  if (_key.equals(this.start)) {
                     followSet.add("$");
                   } else {
                     HashSet<String> tempFollowSet = this.getFollowSet(_key);
                     Iterator _itr = tempFollowSet.iterator();
-                    while(_itr.hasNext()) {
+                    while (_itr.hasNext()) {
                       followSet.add(_itr.next().toString());
                     }
                   }
@@ -129,10 +136,10 @@ public class Yacc {
       HashSet<String> firstWorldFollowSet = new HashSet<>();
       firstWorldFollowSet.add("$");
       this.follow.put(this.start, firstWorldFollowSet);
-      while(iter.hasNext()) {
+      while (iter.hasNext()) {
         String key = iter.next().toString();
-        if(!this.follow.containsKey(key)) {
-          if(!key.equals(this.start)) {
+        if (!this.follow.containsKey(key)) {
+          if (!key.equals(this.start)) {
             HashSet<String> tempFollowSet = this.getFollowSet(key);
             this.follow.put(key, tempFollowSet);
           }
@@ -142,61 +149,62 @@ public class Yacc {
     }
 
     public Analysis(String[] texts) {
+      this.terminator.add("$");
       for (int i = 0; i < texts.length; i++) {
         String[] splitArray = texts[i].replaceAll(" ", "").split("::=");
         Node node = new Node(splitArray[0].replaceAll("<|>", ""));
         node.split(splitArray[1]);
-        if(i == 0) {
+        if (i == 0) {
           this.start = splitArray[0].replaceAll("<|>", "");
         }
         this.bnf.put(splitArray[0].replaceAll("<|>", ""), node);
       }
-      // Iterator iter = this.bnf.keySet().iterator();
-      // while (iter.hasNext()) {
-      //   String key = iter.next().toString();
-      //   System.out.println(key + " : ");
-      //   Node node = this.bnf.get(key);
-      //   ArrayList<String[]> childs = node.getChilds();
-      //   for (int i = 0; i < childs.size(); i++) {
-      //     System.out.print(i + " : ");
-      //     for (int j = 0; j < childs.get(i).length; j++) {
-      //       System.out.print(childs.get(i)[j] + " ");
-      //     }
-      //     System.out.println();
-      //   }
-      // }
-    }
-
-    public Boolean isLL1 () {
-      for(int i = 0; i < this.mutilChoose.size(); i ++) {
-        String key = this.mutilChoose.get(i);
-        System.out.println(key);
-        ArrayList<HashSet<String>> arr = this.getMultiFirstSet(key);
-        for(int j = 0; j < arr.size(); j++) {
-          Iterator itr = arr.get(j).iterator();
-          while(itr.hasNext()) {
-            System.out.println(itr.next().toString());
+      Iterator iter = this.bnf.keySet().iterator();
+      while (iter.hasNext()) {
+        String key = iter.next().toString();
+        this.notTerminator.add(key);
+        Node node = this.bnf.get(key);
+        ArrayList<String[]> childs = node.getChilds();
+        for (int i = 0; i < childs.size(); i++) {
+          for (int j = 0; j < childs.get(i).length; j++) {
+            if (this.isTerminator(childs.get(i)[j])) {
+              this.terminator.add(childs.get(i)[j]);
+            }
           }
         }
-        for(int j = 0; j < arr.size(); j++) {
-          for(int z = j + 1; z < arr.size(); z++) {
+      }
+    }
+
+    public Boolean isLL1() {
+      for (int i = 0; i < this.mutilChoose.size(); i++) {
+        String key = this.mutilChoose.get(i);
+        // System.out.println(key);
+        ArrayList<HashSet<String>> arr = this.getMultiFirstSet(key);
+        // for(int j = 0; j < arr.size(); j++) {
+        //   Iterator itr = arr.get(j).iterator();
+        //   while(itr.hasNext()) {
+        //     System.out.println(itr.next().toString());
+        //   }
+        // }
+        for (int j = 0; j < arr.size(); j++) {
+          for (int z = j + 1; z < arr.size(); z++) {
             HashSet<String> result = new HashSet<>();
             result.addAll(arr.get(j));
             result.retainAll(arr.get(z));
-            if(result.size() > 0) {
+            if (result.size() > 0) {
               return false;
             }
           }
         }
-        for(int j = 0; j < arr.size(); j++) {
-          if(arr.get(j).contains("ε")) {
+        for (int j = 0; j < arr.size(); j++) {
+          if (arr.get(j).contains("ε")) {
             HashSet<String> follow = this.follow.get(key);
-            for(int z = 0; z < arr.size(); z++) {
+            for (int z = 0; z < arr.size(); z++) {
               if (j != z) {
                 HashSet<String> result = new HashSet<>();
                 result.addAll(follow);
                 result.retainAll(arr.get(z));
-                if(result.size() > 0) {
+                if (result.size() > 0) {
                   return false;
                 }
               }
@@ -207,18 +215,64 @@ public class Yacc {
       return true;
     }
 
-    private ArrayList<HashSet<String>> getMultiFirstSet (String key) {
+    public void setLL1Table() {
+      Iterator itr = this.notTerminator.iterator();
+      while (itr.hasNext()) {
+        String key = itr.next().toString();
+        Node node = this.bnf.get(key);
+        ArrayList<String[]> childs = node.getChilds();
+        for (int i = 0; i < childs.size(); i++) {
+          if (this.isTerminator(childs.get(i)[0])) {
+            this.insertLL1Table(key, childs.get(i)[0], childs.get(i));
+          } else {
+            HashSet<String> tempFirstSet = this.first.get(childs.get(i)[0]);
+            Iterator _itr = tempFirstSet.iterator();
+            while (_itr.hasNext()) {
+              String tempKey = _itr.next().toString();
+              this.insertLL1Table(key, tempKey, childs.get(i));
+            }
+          }
+        }
+      }
+    }
+
+    private void insertLL1Table(String key, String key2, String[] value) {
+      if (this.ll1Table.containsKey(key)) {
+        HashMap map1 = this.ll1Table.get(key);
+        if (map1.containsKey(key2)) {
+          HashMap<String, ArrayList<String []>> map2 = map1.get(key2);
+          ArrayList<String[]> tempList = map2.get(key2);
+          tempList.add(value);
+        } else {
+          HashMap<String, ArrayList<String[]>> map2 = new HashMap<>();
+          ArrayList<String[]> tempList = new ArrayList<>();
+          tempList.add(value);
+          map2.put(key2, tempList);
+          map1.put(key, map2);
+        }
+      } else {
+        HashMap<String, String[]> map1 = new HashMap<>();
+        HashMap<String, ArrayList<String[]>> map2 = new HashMap<>();
+        ArrayList<String[]> tempList = new ArrayList<>();
+        tempList.add(value);
+        map2.put(key2, tempList);
+        map1.put(key, map2);
+        this.ll1Table.put(key, map1);
+      }
+    }
+
+    private ArrayList<HashSet<String>> getMultiFirstSet(String key) {
       ArrayList<HashSet<String>> arr = new ArrayList<>();
       Node node = this.bnf.get(key);
-      ArrayList<String []> childs = node.getChilds();
-      for(int i = 0; i < childs.size(); i ++) {
+      ArrayList<String[]> childs = node.getChilds();
+      for (int i = 0; i < childs.size(); i++) {
         HashSet<String> set = new HashSet<>();
         if (this.isTerminator(childs.get(i)[0])) {
           set.add(childs.get(i)[0]);
         } else {
           HashSet<String> tempSet = this.first.get(childs.get(i)[0]);
           Iterator itr = tempSet.iterator();
-          while(itr.hasNext()) {
+          while (itr.hasNext()) {
             set.add(itr.next().toString());
           }
         }
